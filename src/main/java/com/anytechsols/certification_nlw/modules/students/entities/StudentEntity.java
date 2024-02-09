e539e7830cab2793302e8317d614912cb595b648
Norberto Boa <nboa.26@gmail.com>
2024-02-10 01:06:02 +0200

    setup the first table

--- src/main/java/com/anytechsols/certification_nlw/modules/students/entities/StudentEntity.java
+++ src/main/java/com/anytechsols/certification_nlw/modules/students/entities/StudentEntity.java
@@ -3,6 +3,11 @@ package com.anytechsols.certification_nlw.modules.students.entities;
 import java.util.List;
 import java.util.UUID;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,8 +15,15 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity(name = "students")
 public class StudentEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
   private UUID id;
+
+  @Column(unique = true, nullable = false)
   private String email;
-  private List<CertificationStudentEntity> certificationStudentEntity;
+
+  // private List<CertificationStudentEntity> certificationStudentEntity;
 }
